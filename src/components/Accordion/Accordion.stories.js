eba33d39bb0711765580c35cef51f4be2529e2a4
cicomsa <ioanacomsa85@gmail.com>
2020-09-02 21:21:28 +0100

    accordion story

---
+++ src/components/Accordion/Accordion.stories.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Accordion from './index';
+
+
+export default {
+  title: 'Example/Accordion',
+  component: Accordion
+};
+
+const Template = (args) => <Accordion />;
+
+export const Main = Template.bind({});
+
