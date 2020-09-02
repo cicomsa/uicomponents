76195d67530ea4a0b9e1ee5ab8c54fe8fcad747b
cicomsa <ioanacomsa85@gmail.com>
2020-09-02 20:36:39 +0100

    added style transitions on open and close

--- src/components/Accordion/index.js
+++ src/components/Accordion/index.js
@@ -1,8 +1,15 @@
-import React, { useState } from 'react'
+import React, { useState, useRef } from 'react'
 import './index.scss'
 
 const Accordion = () => {
   const [state, setState] = useState([])
+  const [heightState, setHeightState] = useState({
+    contentOne: '0',
+    contentTwo: '0'
+  });
+
+  const content1 = useRef(null)
+  const content2 = useRef(null)
 
   const handleClick = e => {
     const { name } = e.target
@@ -14,26 +21,36 @@ const Accordion = () => {
         array.splice(index, 1);
       }
       setState(array)
+      setHeightState({ ...heightState, [name]: '0' })
     } else {
       setState([...state, name])
+      setHeightState({
+        ...heightState,
+        [name]: name === 'contentOne'
+          ? `${content1.current.scrollHeight}px`
+          : `${content2.current.scrollHeight}px`
+      });
     }
   }
 
   return (
-    <div>
+    <>
       <div className={state.includes('contentOne') ? 'open' : 'closed'}>
         <button name="contentOne" onClick={handleClick}>Open content 1</button>
-        <div className="content">
+        <div className="content" ref={content1}
+          style={{ height: `${heightState.contentOne}` }}>
           Content 1
         </div>
       </div>
       <div className={state.includes('contentTwo') ? 'open' : 'closed'}>
         <button name="contentTwo" onClick={handleClick}>Open content 2</button>
-        <div className="content">
-          Content 2
+        <div className="content" ref={content2}
+          style={{ height: `${heightState.contentTwo}` }}>
+          <p>Content 2</p>
+          <p>Content 2</p>
         </div>
       </div>
-    </div>
+    </>
   )
 }
 
