5e4e7540bde07ce6d4271bb21a970ae165a65b4b
cicomsa <ioanacomsa85@gmail.com>
2020-09-02 15:22:56 +0100

    open and close accordion section on section button click only

--- src/components/Accordion/index.js
+++ src/components/Accordion/index.js
@@ -2,22 +2,32 @@ import React, { useState } from 'react'
 import './index.scss'
 
 const Accordion = () => {
-  const [state, setState] = useState({ open: '' })
+  const [state, setState] = useState([])
 
   const handleClick = e => {
     const { name } = e.target
 
-    setState({ open: name })
+    if (state.includes(name)) {
+      const array = [...state]
+      const index = state.indexOf(name);
+      if (index > -1) {
+        array.splice(index, 1);
+      }
+      setState(array)
+    } else {
+      setState([...state, name])
+    }
   }
+
   return (
     <div>
-      <div className={state.open === 'contentOne' ? 'open' : 'closed'}>
+      <div className={state.includes('contentOne') ? 'open' : 'closed'}>
         <button name="contentOne" onClick={handleClick}>Open content 1</button>
         <div className="content">
           Content 1
         </div>
       </div>
-      <div className={state.open === 'contentTwo' ? 'open' : 'closed'}>
+      <div className={state.includes('contentTwo') ? 'open' : 'closed'}>
         <button name="contentTwo" onClick={handleClick}>Open content 2</button>
         <div className="content">
           Content 2
